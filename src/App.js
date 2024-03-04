bff0aee6505a9531f4acbe117d558d2155e67385
Quiatus <Quiatus@gmail.com>
2024-03-04 19:57:09 +0100

    add axion api

--- src/App.js
+++ src/App.js
@@ -7,27 +7,32 @@ import Missing from './Missing';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { format } from 'date-fns';
+import api from './api/posts';
 
 function App() {
-	const [posts, setPosts] = useState([
-		{
-			id: 1,
-			title: "First Post",
-			datetime: "March 01, 2024 12:05:18",
-			body: "Lorem ipsum dolor sit amet."
-		},
-		{
-			id: 2,
-			title: "Second Post",
-			datetime: "March 02, 2024 12:05:18",
-			body: "Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. "
-		}
-	])
+	const [posts, setPosts] = useState([])
 	const [search, setSearch] = useState('');
 	const [searchResults, setSearchResults] = useState([]);
 	const [postTitle, setPostTitle] = useState('');
 	const [postBody, setPostBody] = useState('');
 	const navigate = useNavigate();
+
+	useEffect(() => {
+		const fetchPosts = async () => {
+			try {
+				const response = await api.get('/posts')
+				setPosts(response.data)
+			} catch (err) {
+				if (err.response) {
+					console.log(err.response.data)
+					console.log(err.response.status)
+					console.log(err.response.headers)
+				} else {
+					console.log(`Error: ${err.message}`)
+				}
+			}
+		}
+	}, [])
 
 	useEffect(() => {
 		const filteredResults = posts.filter(post => ((post.body).toLowerCase()).includes(search.toLowerCase())
@@ -38,7 +43,7 @@ function App() {
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
-		const datetime = format(new Date(), 'dd MMMM, yyyy pp');
+		const datetime = format(new Date(), 'dd MMMM, yyyy HH:mm');
 		const newPost = { id, title: postTitle, datetime, body: postBody}
 		const allPosts = [ ...posts, newPost]
 		setPosts(allPosts)
